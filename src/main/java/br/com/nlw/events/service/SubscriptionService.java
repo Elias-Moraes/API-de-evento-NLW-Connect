60daeac44c7d01cf3711f31aeddf3be892aa3fd1
Elias_M <eliasmoraes0089@gmail.com>
2025-02-23 21:13:47 -0300

    Aula 3.

--- src/main/java/br/com/nlw/events/service/SubscriptionService.java
+++ src/main/java/br/com/nlw/events/service/SubscriptionService.java
@@ -1,5 +1,7 @@
 package br.com.nlw.events.service;
 
+import br.com.nlw.events.dto.SubscriptionRankingByUser;
+import br.com.nlw.events.dto.SubscriptionRankingItem;
 import br.com.nlw.events.dto.SubscriptionResponse;
 import br.com.nlw.events.exception.EventNotFoundException;
 import br.com.nlw.events.exception.SubscriptionConflictException;
@@ -12,6 +14,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import br.com.nlw.events.model.User;
 import br.com.nlw.events.model.Subscription;
+
+import java.util.List;
+import java.util.stream.IntStream;
 
 @Service
 public class SubscriptionService {
@@ -41,11 +46,17 @@ public class SubscriptionService {
             userRec = userRepo.save(user);
         }
 
-        User indicador = userRepo.findById(userId).orElse(null);
-        if (indicador == null) {
-            throw new UserIndicadorNotFoundException("Usuário" + userId + "indicador não existe.");
+        User indicador = null;
+        if (userId != null) {
+            indicador = userRepo.findById(userId).orElse(null);
+
+            if (indicador == null) {
+                throw new UserIndicadorNotFoundException("Usuário" + userId + "indicador não existe.");
+
+            }
 
         }
+
 
         Subscription subs = new Subscription();
         subs.setEvent(evt);
@@ -61,6 +72,35 @@ public class SubscriptionService {
         Subscription resultado = subRepo.save(subs);
         return new SubscriptionResponse(resultado.getSubscriptionNumber(), "http://codecraft.com/"
                 + resultado.getEvent().getPrettyName() + "/" + resultado.getSubscriber().getId());
+
+    }
+
+    public List<SubscriptionRankingItem> getCompleteRanking(String prettyName) {
+        Event evt = evtRepo.findByPrettyName(prettyName);
+        if (evt == null) {
+
+            throw new EventNotFoundException("Ranking do evento " + prettyName + " não existe");
+
+        }
+
+        return subRepo.generateRanking(evt.getEventId());
+    }
+
+    public SubscriptionRankingByUser getRankingByUser(String prettyName, Integer userId) {
+
+        List<SubscriptionRankingItem> ranking = getCompleteRanking(prettyName);
+
+        SubscriptionRankingItem item = ranking.stream().filter(i -> i.userId().equals(userId)).findFirst().orElse(null);
+
+        if (item == null) {
+            throw new UserIndicadorNotFoundException("Não há inscrições com indicação do usuário " + userId);
+        }
+
+        Integer posicao = IntStream.range(0, ranking.size())
+                .filter(pos -> ranking.get(pos).userId().equals(userId))
+                .findFirst().getAsInt();
+
+        return new SubscriptionRankingByUser(item, posicao + 1);
 
     }
 }
